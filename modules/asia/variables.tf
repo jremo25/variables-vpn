d8771075ea7863c315c82e239d987a0c518dcee4
jremo25 <100971526+jremo25@users.noreply.github.com>
2024-05-14 18:23:57 +0100

    Update variables.tf

--- modules/asia/variables.tf
+++ modules/asia/variables.tf
@@ -1,3 +1,20 @@
+variable "asia_network_config" {
+  type = object({
+    network_name             = string
+    auto_create_subnetworks  = bool
+    subnet_name              = string
+    subnet_cidr              = string
+    region                   = string
+    private_ip_google_access = bool
+    firewall = object({
+      name            = string
+      ports           = list(string)
+      source_ranges   = list(string)
+      target_tags     = list(string)
+    })
+  })
+}
+
 variable "asia_vm_config" {
   type = object({
     name         = string
